0dd96adfc33924a3b265e120bc688df46d8321ba
Phong <ngo.minh.phong@framgia.com>
2018-11-29 10:25:40 +0700

    Finish chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by params[:id]
+    @user = User.find_by id: params[:id]
     redirect_to signup_path if @user.nil?
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
   end
 end
